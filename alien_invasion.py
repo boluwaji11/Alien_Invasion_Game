818b47884f7bca205ace48e710655f3922653783
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-02-28 23:29:33 -0600

    added fullscreen settings as a comment

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,7 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()  # An instance of the Settings class
 
-        # Set screen size to fullscreen and game title
+        # Set screen size to fullscreen
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
