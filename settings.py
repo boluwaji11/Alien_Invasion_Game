7110aedc14b3d3455fa2830e6276486ea096d08a
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-02 21:18:54 -0600

    Bullet settings added

--- settings.py
+++ settings.py
@@ -9,3 +9,9 @@ class Settings:
 
         # Ship speed setting. Ship moves by 1.5 pixels
         self.ship_speed = 1.5
+
+        # Bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)  # Dark grey color
