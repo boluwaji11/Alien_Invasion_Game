a4aabf0f9f29285b4fd21d452d783fc32597bc19
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-04 17:58:13 -0600

    ship limit set

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ class Settings:
 
         # Ship speed setting. Ship moves by 1.5 pixels
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet Settings
         self.bullet_speed = 1.5
