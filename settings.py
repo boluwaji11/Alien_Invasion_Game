8dd9ce1288c9d78f6bbc2e67bd190899fb354602
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-04 17:25:13 -0600

    testing new bullet speed

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet Settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3000
+        self.bullet_speed = 1.5
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)  # Dark grey color
         self.bullets_allowed = 3  # limited bullets
