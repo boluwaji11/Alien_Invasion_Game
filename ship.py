10de9b78805f5a034fc482680a4630e02e4c69e3
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-04 18:19:51 -0600

    add center_ship method

--- ship.py
+++ ship.py
@@ -48,3 +48,8 @@ class Ship:
     def blitme(self):
         # Draw the ship at its current location
         self.screen.blit(self.image, self.rect)
+
+    # Center the ship on the screen
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
