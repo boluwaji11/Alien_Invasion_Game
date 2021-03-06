623087c83a9ba8e44e21ccdc6b6111316d0e4ef7
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-06 12:01:06 -0600

    comments added

--- scoreboard.py
+++ scoreboard.py
@@ -18,12 +18,13 @@ class Scoreboard:
 
     # Turn the score into a rendered image
     def prep_score(self):
-        score_str = str(self.stats.score)
+        score_str = str(self.stats.score)  # converted to string
         self.score_image = self.font.render(
             score_str, True, self.text_color, self.settings.bg_color
         )
 
         # Display the score at the top right of the screen
         self.score_rect = self.score_image.get_rect()
+        # 20 pixel from the right of the screen
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
