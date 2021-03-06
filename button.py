73bcc6549031a2051ead1f44c60ef34cffc51e63
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-05 22:26:16 -0600

    comments added

--- button.py
+++ button.py
@@ -1,4 +1,4 @@
-import pygame.font
+import pygame.font  # Lets pygame render text to screen
 
 
 class Button:
@@ -9,13 +9,13 @@ class Button:
 
         # Set the dimensions and properties of the button
         self.width, self.height = 200, 50
-        self.button_color = (0, 255, 0)
-        self.text_color = (255, 255, 255)
-        self.font = pygame.font.SysFont(None, 48)
+        self.button_color = (0, 255, 0)  # Bright green color
+        self.text_color = (255, 255, 255)  # White color
+        self.font = pygame.font.SysFont(None, 48)  # (Default font, font size)
 
         # Build the button's rect object and center it
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
 
-        # The button message needs to be prepped only once
+        # The button message needs to be prepped only once. Handles the text rendering on the button
         self._prep_msg(msg)
