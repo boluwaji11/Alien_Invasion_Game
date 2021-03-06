e21c1804e43a6fdb6acd01485184f2cedff5a0bc
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-06 12:56:55 -0600

    prep_high_score added

--- scoreboard.py
+++ scoreboard.py
@@ -13,13 +13,14 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Prepare the initial score image
+        # Prepare the initial score images
         self.prep_score()
+        self.prep_high_score()
 
     # Turn the score into a rendered image
     def prep_score(self):
-        rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score)
+        rounded_score = round(self.stats.score, -1)  # round to the nearest 10
+        score_str = "{:,}".format(rounded_score)  # Add commas
         self.score_image = self.font.render(
             score_str, True, self.text_color, self.settings.bg_color
         )
@@ -29,6 +30,19 @@ class Scoreboard:
         # 20 pixel from the right of the screen
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
+
+    # Turn the high score into a rendered image
+    def prep_high_score(self):
+        high_score = round(self.stats.high_score, -1)  # round to the nearest 10
+        high_score_str = "{:,}".format(high_score)  # Add commas
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # Center the high score at the top of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
 
     # Draw score to the screen
     def show_score(self):
