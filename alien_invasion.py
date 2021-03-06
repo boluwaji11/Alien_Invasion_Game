3cc5d036e678f624d8cbe803b72a746de3b9ac91
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-06 11:36:16 -0600

    reset game settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -90,7 +90,8 @@ class AlienInvasion:
         # Start a new game when the player clicks play
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
-            # Reset the game statistics
+            # Reset the game settings
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
