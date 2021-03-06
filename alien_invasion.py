484c6d8266457eac176412dcaca5b9dabfc26a2d
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-05 23:00:43 -0600

    deactivated the play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,7 +88,8 @@ class AlienInvasion:
     # Method to hold the mouse click on play button
     def _check_play_button(self, mouse_pos):
         # Start a new game when the player clicks play
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             # Reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
