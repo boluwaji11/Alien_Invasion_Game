d8dbd9ce7fddfb19e4e23d42aaedc4a1291e8651
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-05 22:04:55 -0600

    set game_active to ture

--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # Start Alien Invasion in an active state
+        self.game_active = True
+
     # Initialize statistics that can change during the game
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
