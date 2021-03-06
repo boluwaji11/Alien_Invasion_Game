7be995e6584338043164371c79516e9c9af1364d
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-06 11:32:51 -0600

    added game leveling up settings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # A class to store all settings for Alien Invasion
 class Settings:
-    # Initialize the game settings
+    # Initialize the game's static settings
     def __init__(self):
         # Screen settings
         self.screen_width = 1200
@@ -19,7 +19,26 @@ class Settings:
         self.bullets_allowed = 3  # limited bullets
 
         # Alien Settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10  # Controls how quickly the fleet drops down
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1  # Increases the speed at a reasonable pace
+
+        # Call the dynamic settings
+        self.initialize_dynamic_settings()
+
+    # Initialize settings that change throughout the game
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
         # fleet_direction of 1 represents right; -1 represents left
+        # So the aliens move right at the beginning of a new game
         self.fleet_direction = 1
+
+    # Increase speed settings
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
