5898daaa1e0bdd0b7600f3048746fb2a8c581898
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-05 22:55:18 -0600

    reset the play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -89,7 +89,17 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         # Start a new game when the player clicks play
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset the game statistics
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
     # Method to hold the KEYDOWN events
     def _check_keydown_events(self, event):
