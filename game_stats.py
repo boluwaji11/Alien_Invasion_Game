18ee87a2f47b783cf166bc07319caa86696c6a1a
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-06 12:51:38 -0600

    initialize high score

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,9 @@ class GameStats:
         # Start Alien Invasion in an inactive state
         self.game_active = False
 
+        # High score should never be reset
+        self.high_score = 0
+
     # Initialize statistics that can change during the game
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
