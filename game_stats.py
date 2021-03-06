a8502729fcfe979e415382db8a77058a07b13823
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-06 11:42:09 -0600

    added the score attribute

--- game_stats.py
+++ game_stats.py
@@ -11,3 +11,4 @@ class GameStats:
     # Initialize statistics that can change during the game
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
+        self.score = 0
