1d8f97782779be2bf1e5e65e19ffb987e9e7d876
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-06 13:22:41 -0600

    updated levels on screen

--- scoreboard.py
+++ scoreboard.py
@@ -13,9 +13,10 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Prepare the initial score images
+        # Prepare the initial score images and level
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     # Turn the score into a rendered image
     def prep_score(self):
@@ -44,13 +45,28 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
+    # Turn the level into a rendered image
+    def prep_level(self):
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(
+            level_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # Position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = (
+            self.score_rect.bottom + 10
+        )  # 10 pixels below the current score
+
     # Check to see if there's a new high score
     def check_high_score(self):
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
 
-    # Draw scores to the screen
+    # Draw scores and level to the screen
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
